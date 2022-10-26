81e565273d0ef10c2aba2e93323df8e9f064d4c8
Lucas <bithome1@outlook.com>
2022-10-26 20:03:38 -0300

    removido arquivos da trybe

--- desafio7.sql
+++
@@ -1,8 +0,0 @@
-select art.artista, alb.album, art.artista_id from SpotifyClone.tabela_album as alb
-inner join SpotifyClone.tabela_artista as art on art.artista_id = alb.artista_id;
-
-select usa.seguindo_artista_id, alb.album ,count(*) 
-from SpotifyClone.tabela_usuario_id_seguindo_artista_id as usa
-inner join SpotifyClone.tabela_album as alb on alb.artista_id = usa.seguindo_artista_id
-group by usa.seguindo_artista_id, alb.album ;
- 
